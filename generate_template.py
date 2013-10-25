431a051253801632bc0db7d935cfdd29d88d9fd5
Yves Xie <yxi389@gmail.com>
2013-10-25 16:19:12 -0500

    ignore

---
+++ generate_template.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import sys
+import json
+import re
+import os
+import time
+
+templates = ["#Unigram"]
+
+vertical_locality = [-2,-1,0,1,2]
+for i in vertical_locality:
+	for j in xrange(26):
+		if j == 0:
+			continue
+		templates.append("U%d%d:%s[%d,%d]"%(i,j,'%x',i,j))
+
+templates.append("#Bigram")
+templates.append("B")
+
+f = open('CRF_template','w')
+for t in templates:
+	print(t, file=f)
+f.close()
---
+++ run_crf.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+# Runs CRF++ trainer and 
+
